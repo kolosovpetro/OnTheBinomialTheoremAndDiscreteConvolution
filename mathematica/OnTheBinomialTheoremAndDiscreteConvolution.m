1ead380b7d73849d5c82d16d117677ab14b643a1
Petro Kolosov <kolosovp94@gmail.com>
2022-01-20 19:59:22 +0100

    mathematica package updated

--- mathematica/OnTheBinomialTheoremAndDiscreteConvolution.m
+++ mathematica/OnTheBinomialTheoremAndDiscreteConvolution.m
@@ -2,81 +2,124 @@
 
 BeginPackage["OnTheBinomialTheoremAndDiscreteConvolution`"]
 
-PiecewisePow::usage= "PiecewisePow[r_, t_, n_] is power function defined for f=n^r, defined for n >= t, otherwise is 0. "
-PiecewisePowDiscConv::usage= "Gives discrete convolution of funtion f[r, t, n]. "
-ContinuousConvf::usage= "Gives cont. convolution of funtion f[r, t, n]. "
-CoeffA::usage= "CoeffA[n,k] produces the k-th coefficient A in n-th row."
-ConvolveSum::usage= "ConvolveSum gives convolution-like sum of power."
-L::usage= "L[m, n, k] gives polynomial L. L[m, n, k] gives an integer value of polynomial L with repsect to m, n, k. For m=1 see triangle https://oeis.org/A287326."
-X::usage= "X[m_, t_, a_, b_] gives coefficient X."
-H::usage= "H[m_, t_, k_] gives coefficient H."
-P::usage= "P[m_, n_, a_, b_] gives the polynomial P."
-P2::usage= "P2 = Indicator * P."
-S::usage= "S gives an ordinary power sum."
-DiscConvTable::usage= "DiscConvTable gives a discrete self-convolution of power n for n>=0."
-MatrixPolynomialL::usage= "MatrixPolynomialL gives a MxN matrix of values of polynomial L."
-MacaulayPow::usage= "gives a Macaulay condiditons of x>=a."
-MacaulayPowStrict::usage= "gives a Macaulay condiditons of x>a."
-MacaulayDiscConv::usage= "gives a discrete convolution of MacaulayPow."
-MacaulayDiscConvStrict::usage= "gives a discrete convolution of MacaulayPowStrict."
-MacaulayDiscConvTable::usage= "gives a table consisting of discrete convolution of MacaulayPow."
-MacaulayDiscConvTableStrict::usage= "gives a table consisting of discrete convolution of MacaulayPowStrict."
-GeneralP::usage= "gives the generalized polynomial P."
-BinPoly::usage= "Gives a binomial coefficient (n,k) in polynomial form. "
-Pow::usage= "Pow gives a power function in terms of product of Iverson bracket and argument to floor power."
-ConvPowerIdentity::usage= "Verifies the convolution and binomial expansion identity."
-ConvPowerIdentityStrict::usage= "Verifies the convolution and binomial expansion identity."
-ConvPowerIdentityParametric::usage= "Verifies the convolution and binomial expansion identity."
-ConvPowerIdentityStrictParametric::usage= "Verifies the convolution and binomial expansion identity."
-Iverson::usage= "Gives a true if s is even, false if s is odd. "
+PiecewiseDefinedPowerFunction::usage= "Returns the power function defined as f = n^r for n >= t, otherwise defined as zero."
+
+DiscreteConvolutionOfPiecewiseDefinedPowerFunction::usage= "Returns the discrete convolution of the piecewise defined power funtion f[r, t, n]."
+
+ContinuousConvolutionOfPiecewiseDefinedPowerFunction::usage= "Gives cont. convolution of funtion f[r, t, n]. "
+
+A::usage= "Returns the real coefficient A[n, k] for non-negative ingeters n, k such that n <= k. See definition "
+
+DiscreteConvolutionLikeSumOfPowers::usage= "Returns convolution-like sum of power defined as Sum[k^r (n - k)^r, {k, 0, b-1}]."
+
+PolynomialL::usage= "Returns the polynomial L(m, n, k) for m >= 0 integer, k <= n non-negrative integers. See definition "
+
+PolynomialX::usage= "Returns the polynomial X(m, t, a, b). See definition "
+
+PolynomialH::usage= "Returns the polynomial H[m, t, k]. See definition "
+
+PolynomialP::usage= "Returns the polynomial P[m, b, x]. See definition "
+
+OrdinaryPowerSumS::usage= "Returns ordinary power sum S[p_, n_]:= Sum[k^p, {k, 0, n-1}]."
+
+DiscreteSelfConvolutionOfPowerFunction::usage= "Returns the discrete self-convolution of power function n^k for n>=0."
+
+MatrixPolynomialL::usage= "Returns the MxN matrix filled with values of polynomial L(m, n, k)."
+
+MacaulayPowerFunction::usage= "Returns the Macaulay power function that satisfies the condiditons x >= a."
+
+MacaulayPowerFunctionStrict::usage= "Returns the Macaulay power function that sarisfies the condiditons x > a."
+
+DiscreteConvolutionOfMacaulayPowerFunction::usage= "Returns the discrete convolution of MacaulayPowerFunction."
+
+DiscreteConvolutionOfMacaulayPowerFunctionStrict::usage= "Returns the discrete convolution of MacaulayPowerFunctionStrict."
+
+DiscreteConvolutionOfMacaulayPowerFunctionTable::usage= "Returns a table filled by the values of discrete convolution of MacaulayPowerFunction."
+
+DiscreteConvolutionOfMacaulayPowerFunctionStrictTable::usage= "Returns a table filled by the values of discrete convolution of MacaulayPowerFunctionStrict."
+
+GeneralizedPolynomialP::usage= "Returns the generalized polynomial P[m, x, a, b]."
+
+BinomialCoefficientAsPolynomial::usage= "Returns the polynomial form of the binomial coefficient (n, k)."
+
+IversonPowerFunction::usage= "Returns the power function in terms of product of Iverson bracket and argument to floor power."
+
+BinomialTheoremAndDiscreteConvolutionTest::usage= "Validates the relation between binomial theorem and discrete convolution. See corollary ..."
+
+BinomialTheoremAndDiscreteConvolutionStrictTest::usage= "Validates the relation between binomial theorem and discrete convolution. See corollary ..."
+
+DiscreteConvolutionPowerIdentityParametricTest::usage= "Verifies an equation 6.1. See "
+
+DiscreteConvolutionPowerIdentityStrictParametricTest::usage= "Verifies an equation 6.2. See "
+
+IversonBracket::usage= "Returns true if s is even, otherwise false."
 
 Begin["`Private`"]
 
 (* Definitions and conventions *)
 
+(* Defines x^0 == 1 for all x *)
 Unprotect[Power];
 Power[0|0., 0|0.] = 1;
 Protect[Power];
 
-CoeffA[n_, k_] := 0;
-CoeffA[n_, k_] := (2k + 1) * Binomial[2k, k] * Sum[CoeffA[n, j] * Binomial[j, 2k + 1] * (-1)^(j - 1) / (j - k) * BernoulliB[2j - 2k], {j, 2k + 1, n}] /; 0 <= k < n;
-CoeffA[n_, k_] := (2n + 1) * Binomial[2n, n] /; k == n;
+A[n_, k_] := 0;
+A[n_, k_] := (2k + 1) * Binomial[2k, k] * Sum[A[n, j] * Binomial[j, 2k + 1] * (-1)^(j - 1) / (j - k) * BernoulliB[2j - 2k], {j, 2k + 1, n}] /; 0 <= k < n;
+A[n_, k_] := (2n + 1) * Binomial[2n, n] /; k == n;
 
-L[m_, n_, k_] := Sum[CoeffA[m, r] * k^r * (n - k)^r, {r, 0, m}];
-P[m_, n_, b_] := Sum[L[m, n, k], {k, 0, b - 1}];
-H[m_, t_, b_] := Sum[Binomial[j, t] * CoeffA[m, j] * (-1)^j / (2j - t + 1) * Binomial[2j - t + 1, b] * BernoulliB[2j - t + 1 - b], {j, t, m}];
-X[m_, t_, j_] := Sum[(-1)^m * H[m, t, k] * j^k, {k, 1, 2m - t + 1}];
-S[p_, n_]:= Sum[k^p, {k, 0, n-1}];
+PolynomialL[m_, n_, k_] := Sum[A[m, r] * k ^ r * (n - k)^r, {r, 0, m}];
 
-ConvolveSum[n_, r_, b_] := Sum[k^r (n - k)^r, {k, 0, b - 1}];
-PiecewisePow[x_, n_, a_] := x^n * Boole[x >= a];
-MacaulayPow[x_, n_, a_] := Piecewise[{{(x - a)^n, x >= a}, {0, True}}];
-MacaulayPowStrict[x_, n_, a_] := Piecewise[{{(x - a)^n, x > a}, {0, True}}];
+PolynomialP[m_, n_, b_] := Sum[PolynomialL[m, n, k], {k, 0, b-1}];
+
+PolynomialH[m_, t_, b_] := Sum[Binomial[j, t] * A[m, j] * (-1)^j / (2j - t + 1) * Binomial[2j - t + 1, b] * BernoulliB[2j - t + 1 - b], {j, t, m}];
+
+PolynomialX[m_, t_, j_] := Sum[(-1)^m * PolynomialH[m, t, k] * j^k, {k, 1, 2m - t + 1}];
+
+OrdinaryPowerSumS[p_, n_]:= Sum[k^p, {k, 0, n-1}];
+
+DiscreteConvolutionLikeSumOfPowers[n_, r_, b_] := Sum[k^r (n - k)^r, {k, 0, b - 1}];
+
+PiecewiseDefinedPowerFunction[x_, n_, a_] := x^n * Boole[x >= a];
+
+MacaulayPowerFunction[x_, n_, a_] := Piecewise[{{(x - a) ^ n, x >= a}, {0, True}}];
+
+MacaulayPowerFunctionStrict[x_, n_, a_] := Piecewise[{{(x - a)^n, x > a}, {0, True}}];
+
 Iverson[s_] := Boole[Mod[s, 2] == 0];
-Pow[x_, s_] := x^Iverson[s] * x^(2 Floor[(s - 1) / 2] + 1);
 
-PiecewisePowDiscConv[x_, n_, a_] := Sum[PiecewisePow[k, n, a] * PiecewisePow[x - k, n, a], {k, -Infinity, +Infinity}];
-MacaulayDiscConv[x_, n_, a_] := Sum[MacaulayPow[k, n, a] * MacaulayPow[x - k, n, a], {k, -Infinity, +Infinity}];
-MacaulayDiscConvStrict[x_, n_, a_] := Sum[MacaulayPowStrict[k, n, a] * MacaulayPowStrict[x - k, n, a], {k, -Infinity, +Infinity}];
-ContinuousConvf[x_, n_, a_] := Integrate[PiecewisePow[k, n, a] * PiecewisePow[x - k, n, a], {k, -Infinity, +Infinity}];
+IversonPowerFunction[x_, s_] := x^Iverson[s] * x^(2 Floor[(s - 1) / 2] + 1);
 
-P2[m_, n_, b_] := n^Iverson[m] * P[Floor[(m - 1) / 2], n, b];
-GeneralP[m_, n_, a_, b_] := Expand[Sum[L[m, n, k], {k, a, b - 1}]];
-BinPoly[t_, k_] := 1 / k! * Product[(t - w), {w, 0, k - 1}];
+DiscreteConvolutionOfPiecewiseDefinedPowerFunction[x_, n_, a_] := Sum[PiecewiseDefinedPowerFunction[k, n, a] * PiecewiseDefinedPowerFunction[x - k, n, a], {k, -Infinity, +Infinity}];
 
-(* Convolutional tables and data sets *)
+DiscreteConvolutionOfMacaulayPowerFunction[x_, n_, a_] := Sum[MacaulayPowerFunction[k, n, a] * MacaulayPowerFunction[x - k, n, a], {k, -Infinity, +Infinity}];
 
-DiscConvTable[m_, r_] := Column[Table[PiecewisePowDiscConv[n - k, k, r], {n, 0, m}, {k, 0, n}], Left];
-MacaulayDiscConvTable[m_, a_] := Column[Table[MacaulayDiscConv[n - k, k, a], {n, 0, m}, {k, 0, n}], Left];
-MacaulayDiscConvTableStrict[m_, a_] := Column[Table[MacaulayDiscConvStrict[n - k, k, a], {n, 0, m}, {k, 0, n}], Left];
+DiscreteConvolutionOfMacaulayPowerFunctionStrict[x_, n_, a_] := Sum[MacaulayPowerFunctionStrict[k, n, a] * MacaulayPowerFunctionStrict[x - k, n, a], {k, -Infinity, +Infinity}];
+
+ContinuousConvolutionOfPiecewiseDefinedPowerFunction[x_, n_, a_] := Integrate[PiecewiseDefinedPowerFunction[k, n, a] * PiecewiseDefinedPowerFunction[x - k, n, a], {k, -Infinity, +Infinity}];
+
+GeneralizedPolynomialP[m_, n_, a_, b_] := Expand[Sum[L[m, n, k], {k, a, b - 1}]];
+
+BinomialCoefficientAsPolynomial[t_, k_] := 1 / k! * Product[(t - w), {w, 0, k - 1}];
+
+(* Convolutional tables and data sets. *)
+
+DiscreteSelfConvolutionOfPowerFunction[m_, r_] := Column[Table[PiecewisePowDiscConv[n - k, k, r], {n, 0, m}, {k, 0, n}], Left];
+
+DiscreteConvolutionOfMacaulayPowerFunctionTable[m_, a_] := Column[Table[DiscreteConvolutionOfMacaulayPowerFunction[n - k, k, a], {n, 0, m}, {k, 0, n}], Left];
+
+DiscreteConvolutionOfMacaulayPowerFunctionStrictTable[m_, a_] := Column[Table[DiscreteConvolutionOfMacaulayPowerFunctionStrict[n - k, k, a], {n, 0, m}, {k, 0, n}], Left];
+
 MatrixPolynomialL[m_, M_, N_] := Column[Table[L[m, n, k], {n, -N, N}, {k, -M, M}], Left];
 
-(* For Unit tests *)
+(* Expressions and corollaries tests. *)
 
-ConvPowerIdentity[m_, x_] := Refine[Sum[CoeffA[m,r] * MacaulayDiscConv[x, r, 0], {r, 0, m}], Element[x, Integers], Assumptions -> x>0];
-ConvPowerIdentityStrict[m_, x_] := Refine[Sum[CoeffA[m,r] * MacaulayDiscConvStrict[x, r, 0], {r, 0, m}], Element[x, Integers], Assumptions -> x>0];
-ConvPowerIdentityParametric[m_, x_, a_] := Expand[Refine[Sum[CoeffA[m,r] * MacaulayDiscConv[x, r, a], {r, 0, m}], Element[x, Integers], Assumptions -> x>2*a]];
-ConvPowerIdentityStrictParametric[m_, x_, a_] := Expand[Refine[Sum[CoeffA[m,r] * MacaulayDiscConvStrict[x, r, a], {r, 0, m}], Element[x, Integers], Assumptions -> x>2*a]];
+BinomialTheoremAndDiscreteConvolutionTest[m_, x_] := Refine[Sum[A[m,r] * DiscreteConvolutionOfMacaulayPowerFunction[x, r, 0], {r, 0, m}], Element[x, Integers], Assumptions -> x > 0];
+
+BinomialTheoremAndDiscreteConvolutionStrictTest[m_, x_] := Refine[Sum[A[m,r] * DiscreteConvolutionOfMacaulayPowerFunctionStrict[x, r, 0], {r, 0, m}], Element[x, Integers], Assumptions -> x > 0];
+
+DiscreteConvolutionPowerIdentityParametricTest[m_, x_, a_] := Expand[Refine[Sum[A[m,r] * DiscreteConvolutionOfMacaulayPowerFunction[x, r, a], {r, 0, m}], Element[x, Integers], Assumptions -> x > 2a]];
+
+DiscreteConvolutionPowerIdentityStrictParametricTest[m_, x_, a_] := Expand[Refine[Sum[A[m,r] * DiscreteConvolutionOfMacaulayPowerFunctionStrict[x, r, a], {r, 0, m}], Element[x, Integers], Assumptions -> x > 2a]];
 
 End[ ]
 
